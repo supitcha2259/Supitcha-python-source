3c8dd315ba20fb2033bf09d398a09f1d3625227d
Supitcha Yeeviriyakul <supitcha.ye@ku.th>
2026-07-24 12:01:48 +0700

    ATM

---
+++ assignments/week03/Multiplication_ta.py
@@ -0,0 +1,4 @@
+number = int(input("Multiplication table number: "))
+
+for i in range(1, 13):
+    print(f"{number} × {i} = {number * i}")
--- assignments/week03/quiz.py
+++ assignments/week03/quiz.py
@@ -8,8 +8,15 @@ age = int(input("Enter age: "))
 # 60+: Senior
 
 # Your code here:
-
-
+if age >= 0 and age <= 12:
+    print("Child")
+elif age > 12 and age <= 19:
+    print("Teenager")
+elif age > 19 and age <= 59:
+    print("Adult")
+else:
+    print("Senior")
+print("=" * 50)
 
 # Complete this ATM simulation
 balance = 1000
@@ -18,16 +25,37 @@ pin = "1234"
 entered_pin = input("Enter PIN: ")
 if entered_pin == pin:
     print("PIN accepted")
+    print("=" * 50)
     while True:
-        print("\n1. Check Balance")
-        print("2. Withdraw")
-        print("3. Deposit") 
+        print("\n1. Check Balance") #เงินในบัญชี
+        print("2. Withdraw") #ถอนเงิน
+        print("3. Deposit") #ฝากเงิน
         print("4. Exit")
-        
+        print("=" * 50)
         choice = input("Choose option: ")
-        
+        print("=" * 50)
         # Complete the menu logic here
         # Your code here:
-        
+        if choice == "1":
+            print("Balance:", balance, "Bath")
+            print("=" * 50)
+        elif choice == "2":
+            withdraw = int(input("Enter amount: "))
+            if withdraw <= balance:
+                balance = balance - withdraw
+                print("Withdraw successful")
+            else:
+                print("Insufficient funds")
+            print("=" * 50)
+        elif choice == "3":
+            deposit = (float(input("Enter amount to deposit: ")))
+            new_balance = balance + deposit
+            print(f"Deposit success!\nNew balance: {new_balance} Bath")
+            print("=" * 50)
+        elif choice == "4":
+            print("Thank for using!")
+            break
+
 else:
     print("Invalid PIN")
+print("=" * 50)
